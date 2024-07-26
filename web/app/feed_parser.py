1aa8fd03c43c58b6cbbe002068facd7763fce615
bhunao <bhunao@gmail.com>
2024-07-26 15:29:38 -0300

    ADD DOCS for me to know and you too

--- web/app/feed_parser.py
+++ web/app/feed_parser.py
@@ -4,7 +4,7 @@ import feedparser
 
 from collections.abc import Generator
 from sqlmodel import SQLModel
-from typing import Any  # pyright: ignore[reportAny]
+from typing import Any
 
 from app.models import SourceSchema, ArticleSchema
 
@@ -18,7 +18,9 @@ class RssSchema(SQLModel):
     articles: list[ArticleSchema]
 
     @staticmethod
-    def parse_source(parsed_rss: DICT, url: str = "") -> SourceSchema:
+    def source_from_rss(parsed_rss: DICT, url: str = "") -> SourceSchema:
+        """Creates a new instance of the class `SourceSchema` with the data
+        from the parsed rss dictionary."""
         assert parsed_rss.get("feed"), "RSS has no feed key in dict."
 
         feed: DICT = parsed_rss["feed"]
@@ -38,13 +40,13 @@ class RssSchema(SQLModel):
                 raise KeyError("missing something or wawhetversdasdver")
 
     @ staticmethod
-    def parse_articles(parsed_rss: DICT) -> list[ArticleSchema]:
+    def articles_from_rss(parsed_rss: DICT) -> list[ArticleSchema]:
         """Get the articles from the `rss_dict` and create an `ArticleSchema`
         for each one of the valid articles.
 
-        The articles often come with missing fields so all fields must be checked
-        if exists and if its valid before trying to acess and save that data into
-        the `ArticleSchema` object."""
+        The articles often come with missing fields so all fields must be
+        checked if exists and if its valid before trying to acess and save that
+        data into the `ArticleSchema` object."""
         articles: list[ArticleSchema] = []
         for entry in parsed_rss["entries"]:
             entry: dict[str, Any]
@@ -67,6 +69,19 @@ class RssSchema(SQLModel):
 
     @staticmethod
     def rss_dict_from(url: str) -> DICT | Exception:
+        """Parses an RSS feed from a given URL and returns the parsed content.
+
+        This method fetches and parses an RSS feed from the provided URL using
+        the `feedparser` library. If the parsing results
+        returns an exception inside the dictionary then the method will return
+        the error as a value.
+
+        The `feedparser.parse` can accept multiple type of arguments.
+        - url
+        - file
+        - stream
+        - xml as string
+        """
         parsed: DICT = feedparser.parse(url)
         assert isinstance(parsed, dict)
         exception: Exception | None = parsed.get("bozo_exception")
@@ -76,11 +91,17 @@ class RssSchema(SQLModel):
 
     @classmethod
     def from_url(cls, url: str) -> Self | Exception:
+        """Creates a new instance of the class `RssSchema` from the parsed
+        results of the given url.
+
+        This method parses everything needed for the `RssSchema` to be
+        created, if any data necessary for the creation of the `RssSchema`
+        is invalid or not present the method returns the error as a value."""
         rss_dict = cls.rss_dict_from(url)
         if isinstance(rss_dict, Exception):
             return rss_dict
-        source = cls.parse_source(rss_dict, url)
-        articles = cls.parse_articles(rss_dict)
+        source = cls.source_from_rss(rss_dict, url)
+        articles = cls.articles_from_rss(rss_dict)
         return cls(
             source=source,
             articles=articles
