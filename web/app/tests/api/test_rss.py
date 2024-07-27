944f50a62c65afb7651d0fe1d3562a08c912dbdc
bhunao <bhunao@gmail.com>
2024-07-27 17:24:21 -0300

    asserting fields that always in the dictionary

--- web/app/tests/api/test_rss.py
+++ web/app/tests/api/test_rss.py
@@ -21,6 +21,7 @@ INVALID_LINKS = [
 
 def test_rss_link(client: TestClient) -> None:
     for _link in LINKS:
+        print(f"testing link: {_link}")
         data = {"url": _link}
         response = client.post(
             "/rss/parse_from/url",
@@ -50,133 +51,96 @@ def test_parse_feed() -> None:
 
     Test to see all the possible data that came in the xml."""
 
-    all_keys: set[str] = set()
-    missing_in: set[str] = set()
-    always_keys: set[str] = set()
     for _link in LINKS:
+        print(f"testing link: {_link}")
         rss_schema = RssSchema.rss_dict_from(_link)
         assert isinstance(rss_schema, dict)
-        # assert rss_schema["status"] == 200
-        # assert isinstance(rss_schema, RssSchema)
-        # print(rss_schema.keys())
-        # print(["bozo", rss_schema["bozo"]])
-        # print(["feed", rss_schema["feed"].keys()])
-        # print(rss_schema["source"])
+
+        # rss feed
+        match rss_schema:
+            case {
+                "version": version,
+                "bozo": bozo,
+                "status": status,
+                # "etag": etag,
+                # "href": href,
+                # "namespaces": namespaces,
+                # "updated": updated,
+                # "updated_parsed": updated_parsed,
+                # "encoding": encoding
+            }:
+                assert status is not None
+                assert version is not None
+                assert bozo is not None
+                # assert etag is not None
+                # assert href is not None
+                # assert namespaces is not None
+                # assert status is not None
+                # assert updated is not None
+                # assert updated_parsed is not None
+                # assert encoding is not None
+            case _:
+                e = list(rss_schema.keys())
+                assert False, f"Invalid Key inside `rss dictionary`: {e}"
+
+        # source
+        feed = rss_schema["feed"]
+        feed: dict[str, str]
+        match feed:
+            case {
+                "title": title,
+                "title_detail": title_detail,
+                # "link": link,
+                # "links": links,
+                # "subtitle": subtitle,
+                # "language": language,
+                # "rights": rights,
+                # "rights_detail": rights_detail,
+                # "image": image
+            }:
+                assert title
+                assert title_detail
+                print(f"{title_detail =}")
+                # assert link
+                # assert links
+                # assert subtitle
+                # assert language
+                # assert rights
+                # assert rights_detail
+                # assert image
+                pass
+            case _:
+                e = list(feed.keys())
+                assert False, f"Invalid Key inside the `feed`: {e}"
 
         #  articles
         for entry in rss_schema["entries"]:
             entry: dict[str, str | list[dict[str, str]]]
 
-            keys = list(entry.keys())
-            [all_keys.add(name) for name in keys]
-            {missing_in.add(name) for name in all_keys if name not in keys}
-
-            # assert entry["summary"]
-
             match entry:
                 case {
-                    # "tags": [{"term": tags}],
-                    # "title": title,
-                    # "title_detail": title_detail,
                     "summary": summary,
                     "summary_detail": summary_detail,
                     "links": links,
                     "link": link,
+                    # "tags": [{"term": tags}],
+                    # "title": title,
+                    # "title_detail": title_detail,
                     # "published": published,
                     # "published_parsed": published_parsed
                 }:
-                    # assert tags
-                    # assert title
-                    # assert title_detail
                     assert summary_detail is not None
                     assert summary is not None
                     assert links
                     assert link
+                    # assert tags
+                    # assert title
+                    # assert title_detail
                     # assert published
                     # assert published_parsed
                 case _:
                     e = list(entry.keys())
                     assert False, f"Invalid Key inside the `entries`: {e}"
-
-            # source
-            feed = rss_schema["feed"]
-            feed: dict[str, str]
-            match feed:
-                case {
-                    "title": title,
-                    "title_detail": title_detail,
-                    "link": link,
-                    "links": links,
-                    "subtitle": subtitle,
-                    "language": language,
-                    "rights": rights,
-                    "rights_detail": rights_detail,
-                    "image": image
-                }:
-                    assert title
-                    assert title_detail
-                    assert link
-                    assert links
-                    assert subtitle
-                    assert language
-                    assert rights
-                    assert rights_detail
-                    assert image
-                case _:
-                    e = list(entry.keys())
-                    # assert False, f"Invalid Key inside the `feed`: {e}"
-
-            # rss feed
-            match rss_schema:
-                case {
-                    "etag": etag,
-                    "href": href,
-                    "namespaces": namespaces,
-                    "version": version,
-                    "status": status,
-                    "updated": updated,
-                    "updated_parsed": updated_parsed,
-                    "encoding": encoding
-                }:
-                    assert etag is not None
-                    assert href is not None
-                    assert namespaces is not None
-                    assert version is not None
-                    assert status is not None
-                    assert updated is not None
-                    assert updated_parsed is not None
-                    assert encoding is not None
-                case _:
-                    e = list(entry.keys())
-                    # assert False, f"Invalid Key inside `rss dictionary`: {e}"
-
-            # print(rss_schema.keys())
-            # print(rss_schema["etag"])
-            # print("")
-            # print(rss_schema["updated"])
-            # print("")
-            # print(rss_schema["href"])
-            # print("")
-            # print(rss_schema["namespaces"])
-            # print("")
-            # print(rss_schema["version"])
-            # print("")
-            # print(rss_schema["status"])
-            # print("")
-            # print(rss_schema["updated"])
-            # print("")
-            # print(rss_schema["updated_parsed"])
-            # print(type(rss_schema["updated_parsed"]))
-            # print("")
-            # print(rss_schema["encoding"])
-
-    print("")
-    print(f"{all_keys =}")
-    print("")
-    print(f"{missing_in =}")
-    print("")
-    always_keys = {name for name in all_keys if name not in missing_in}
-    print(f"{always_keys =}")
 
 
 def test_parse_feed_invalid_link() -> None:
