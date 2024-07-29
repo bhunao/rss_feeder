c26b0e45c72d61109ae615fa9e5c8e7d4dad5361
bhunao <bhunao@gmail.com>
2024-07-29 06:36:23 -0300

    bootstrap in template

--- web/app/api/routes/rss.py
+++ web/app/api/routes/rss.py
@@ -27,7 +27,24 @@ async def parse_from_url(url: UrlSchema):
     return rss
 
 
-html_template = "<html><div><h1>{content}</h1></div></html>".format
+html_template = """\
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class="container align-content-center p-5">
+        <h1>This is a heading</h1>
+        <p>This is a paragraph.</p>
+        <h3 class="display-3">{content}</h3>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>\
+""".format
 
 
 @router.get("/tst",
@@ -37,8 +54,9 @@ html_template = "<html><div><h1>{content}</h1></div></html>".format
                     "description": "Return the JSON item or HTML.",
                 }
             })
-async def tst(a: int = 0):
-    if a == 0:
-        return HTMLResponse(html_template(content=a))
+async def tst(a: str = "empty"):
+    if len(a) < 5:
+        html = html_template(content=a)
+        return HTMLResponse(html)
     else:
         return JSONResponse({"algo": a})
