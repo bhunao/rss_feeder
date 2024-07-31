352d5d95b4e526fe302254fb592ff0de3113cd6e
bhunao <bhunao@gmail.com>
2024-07-31 15:44:23 -0300

    testing new template with 'select'

--- web/app/api/routes/rss.py
+++ web/app/api/routes/rss.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse, Response
 from sqlmodel import SQLModel
 
 from app.models import RssSchema
@@ -30,10 +30,29 @@ multi_responses = {
 }
 
 
+def multi_response_type(request: Request, model_or_schema: SQLModel | Exception) -> Response | SQLModel:
+    accept_values = request.headers.get("accept", None)
+    if isinstance(accept_values, str) and "application/json" in accept_values:
+        if isinstance(model_or_schema, Exception):
+            raise S400_INVALID_URL
+        else:
+            return model_or_schema
+
+    if isinstance(model_or_schema, Exception):
+        context = {"error": model_or_schema}
+    else:
+        context = {"rss": model_or_schema}
+
+    return templates.TemplateResponse(request, "index.html", context=context)
+
+
 @ router.get("/parse_from", responses=multi_responses)
 async def parse_from_url(request: Request, url: str = EX_URL):
     """Returns a parsed dict(json) from a RSS url (url -> json)"""
     rss = RssSchema.from_url(url)
+
+    return multi_response_type(request, rss)
+
     context = {"rss": rss}
     accept_values = request.headers.get("accept", None)
     assert accept_values
@@ -42,6 +61,7 @@ async def parse_from_url(request: Request, url: str = EX_URL):
             raise S400_INVALID_URL
         return rss
 
-    assert not isinstance(
-        rss, Exception), "Rss is not RssSchema, is an exception."
+    if isinstance(rss, Exception):
+        del context["rss"]
+        context["error"] = rss
     return templates.TemplateResponse(request, "index.html", context=context)
